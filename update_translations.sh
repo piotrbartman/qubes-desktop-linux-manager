e408612a6b9a86a458ec27eb3e1aa1281b4d8147
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-11-01 01:55:23 +0100

    Update update_translations.sh script
    Locales are stored in 'locale' dir, not 'locales'.

--- update_translations.sh
+++ update_translations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-xgettext -d desktop-linux-manager -o locales/desktop-linux-manager.pot qui/*py qui/tray/*py qui/*glade
+xgettext -d desktop-linux-manager -o locale/desktop-linux-manager.pot qui/*py qui/tray/*py qui/*glade
 
-find locales/* -maxdepth 2 -mindepth 2 -type f -iname '*.po' -exec msgmerge --update {} locales/desktop-linux-manager.pot \;
+find locale/* -maxdepth 2 -mindepth 2 -type f -iname '*.po' -exec msgmerge --update {} locale/desktop-linux-manager.pot \;
